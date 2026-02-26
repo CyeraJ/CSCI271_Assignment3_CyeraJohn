ed81734283dc81618cf2d718b0c15a81c584e144
Cyera John <johnc2@winthrop.edu>
2026-02-25 20:28:57 -0500

    first program done

--- RecursionNoloop.java
+++ RecursionNoloop.java
@@ -1,1 +1,28 @@
 //starting
+import java.util.Scanner;
+
+public class RecursionNoloop {
+
+    // Recursive function to calculate length of string
+    public static int length(String s) {
+        // Base case: will be an empty string
+        if (s.equals("")) { //empty string if there was a number here it wouldn't be a base case
+            return 0;
+        }
+        // Recursive case
+        return 1 + length(s.substring(1)); //returns the number of characters in S recursively.
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String S = scanner.nextLine(); //reads the full line including any spaces
+
+        int recursivelength = length(S); //calls the function
+
+        System.out.println("Length of the string: " + recursivelength); //prints function
+
+        scanner.close();
+    }
+}
